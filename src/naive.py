7413ad2478980136239fb89aa5d3d220c7816db2
aMark-Dev <marcos.dev2025@gmail.com>
2026-08-09 00:09:29 -0300

    Only the code for naive.py and sqrt_bound.py.

---
+++ src/naive.py
@@ -0,0 +1,19 @@
+def naive(n):
+    prime_numbers_list = [2]
+
+    k = prime_numbers_list[0] + 1
+
+    while n > len(prime_numbers_list):
+
+        for num in prime_numbers_list:
+
+            if k % num: break
+
+            if num == prime_numbers_list[-1]:
+                prime_numbers_list.append(k)
+                break
+
+        k+=1
+
+        #If you want the list of prime numbers, you do use:
+        #return prime_numbers_list
---
+++ src/sqrt_bound.py
@@ -0,0 +1,19 @@
+def sqrt_bound(n):
+    prime_numbers_list = [2]
+
+    k = prime_numbers_list[0] + 1
+
+    while n > len(prime_numbers_list):
+
+        for num in prime_numbers_list:
+
+            if k % num: break
+
+            if num**2 > k:
+                prime_numbers_list.append(k)
+                break
+
+        k+=1
+
+        #If you want the list of prime numbers, you do use:
+        #return prime_numbers_list
